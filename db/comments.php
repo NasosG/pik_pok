2eafc450c62a9255da3b1150155b04497e64d87e
NasosG <53830262+NasosG@users.noreply.github.com>
2020-06-01 22:44:16 +0300

    Update comments.php

--- db/comments.php
+++ db/comments.php
@@ -4,6 +4,13 @@
 
 	$data = array();
 	$comment_text = $_POST['comment-text'];
+	$uname = $_SESSION['username'];
+	$comment_date = date("Y-m-d H:i:s");
+
+	$query = "INSERT INTO post_comments (post_id , user_id, comment_text, time_commented) VALUES ('1', '$uname', '$comment_text', '$comment_date') "; 											
+	$result = mysqli_query($con, $query);	
+
+
 	// return all our data to an AJAX call
 	$data['success'] = true;
 	$data['message'] = $comment_text;
