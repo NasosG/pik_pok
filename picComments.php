ee65003a68ec98716536f5ff3868d6dd05f52214
NasosG <53830262+NasosG@users.noreply.github.com>
2020-06-11 04:31:37 +0300

    -

--- picComments.php
+++ picComments.php
@@ -23,9 +23,8 @@ $row2 = mysqli_fetch_array($result);
 <html>
 
 <head>
-<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta charset="UTF-8">
 	<link rel='shortcut icon' type='image/x-icon' href='images/logo.png'/>
-    
     <title>Comments - Pik Pok</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="" />
