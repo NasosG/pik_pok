9bd6fa7d476dc1eaf8ccefa0c293c9ae6b086133
Thanasis Galatis <gnasos219@gmail.com>
2020-09-21 03:20:46 +0300

    webcam page made responsible + ui and functionality fixes and changes

--- webcamjs/demos/basic.php
+++ webcamjs/demos/basic.php
@@ -3,18 +3,85 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>WebcamJS Test Page</title>
+	<title>Photo</title>
+
+	<link rel='shortcut icon' type='image/x-icon' href='../../images/logo.png'/>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+
+
+	<!-- font awesome icons kit -->
+	<script src="https://kit.fontawesome.com/fac8ebb301.js" crossorigin="anonymous"></script>
+	<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+
+
 	<style type="text/css">
 		body { font-family: Helvetica, sans-serif; }
 		h2, h3 { margin-top:0; }
 		form { margin-top: 15px; }
 		form > input { margin-right: 15px; }
-		#results { float:right; margin:20px; padding:20px; border:1px solid; background:#ccc; }
+
+		input[type=button],
+		button[type=submit] {
+			background-color: #F0F0F0;
+			font-size:1em;
+			color: black;
+			border: 1px solid black;
+			padding: 14px 20px;
+			margin: 8px 0;
+			transition-duration: 0.4s;
+			//border-radius: 4px;
+			cursor: pointer;
+			margin:5px;
+		}
+
+		input[type=button]:hover {
+			background-color: #555555;
+			border: 1px solid #555555;
+			color: white;
+		}
+
+		button[type=submit]:hover {
+			background-color: green;
+			border: 1px solid green;
+			color: white;
+		}
+
+		.grid-container {
+			display: grid;
+			grid-template-columns: 30% 30%;
+			max-width: 600px;
+			padding: 0px;
+		}
+
+		.grid-item {
+			padding: 0px;
+
+			text-align: center;
+		}
+
+@media only screen and (max-width: 600px) { 
+body {
+ 
+}			
+#results { display:none;} 
+}
+		@media only screen and (min-width: 600px) and (max-width: 1250px){ 
+			body {
+  margin-left:auto;margin-right:auto; max-width:600px;
+}
+			#results { display:none;} 
+	}
+		@media only screen and (min-width: 1251px) { 
+			#results { float:right; margin:20px; padding:20px; border:1px solid; background:#ccc;} 
+	}
 	</style>
 </head>
 <body>
-	<div id="results">Your captured image will appear here...</div>
 	
+	<div id="results">Your captured image will appear here...</div>
 	<h1>Take a photo</h1>
 	<h3>Press Take Snapshot &amp; then submit it</h3>
 	
@@ -25,25 +92,54 @@
 	
 	<!-- Configure a few settings and attach camera -->
 	<script language="JavaScript">
-		Webcam.set({
-			width: 320,
-			height: 240,
+			Webcam.set({
+			// live preview size
+			width: 560,
+			height: 420,
+			
+			// device capture size
+			dest_width: 560,
+			dest_height: 420,
+			
+			// format and quality
 			image_format: 'jpeg',
 			jpeg_quality: 90
 		});
+		
 		Webcam.attach( '#my_camera' );
+
+
+		
 	</script>
 	
 	<!-- A button for taking snaps -->
 	<form action="../../post.php" method="post">
+
 		<input type="hidden" name="imgData" id="imgData" value= "" />
-		<input type=button name="snapshot" id="snapshot" value="Take Snapshot" onClick="take_snapshot()">
-		<button type="submit">Submit Photo</button>
+
+		<div id="pre_take_buttons">
+			<input type=button name="snapshot" id="snapshot" value="Take Snapshot" onClick="preview_snapshot()">
+		</div>
+		<div id="post_take_buttons" style="display:none">
+			<input type=button value="&lt; Take Another" onClick="cancel_preview()">
+			<button type="submit" onClick="save_photo()">Submit Photo &gt;</button>
+		</div>
+		
 	</form>
 	
 	<!-- Code to handle taking the snapshot and displaying it locally -->
 	<script language="JavaScript">
-		function take_snapshot() {
+
+		// preload shutter audio clip
+		var shutter = new Audio();
+		shutter.autoplay = false;
+		shutter.src = navigator.userAgent.match(/Firefox/) ? 'shutter.ogg' : 'shutter.mp3';
+
+/*		function take_snapshot() {
+
+			// play sound effect
+			shutter.play();
+
 			// take snapshot and get image data
 			Webcam.snap( function(data_uri) {
 				// display results in page
@@ -59,6 +155,46 @@
 					
 			} );
 		}
+
+*/
+		function preview_snapshot() {
+			shutter.play();
+			// freeze camera so user can preview pic
+			Webcam.freeze();
+			
+			// swap button sets
+			document.getElementById('pre_take_buttons').style.display = 'none';
+			document.getElementById('post_take_buttons').style.display = '';
+		}
+		
+		function cancel_preview() {
+			save_photo();
+			// cancel preview freeze and return to live camera feed
+			Webcam.unfreeze();
+			
+			// swap buttons back
+			document.getElementById('pre_take_buttons').style.display = '';
+			document.getElementById('post_take_buttons').style.display = 'none';
+			
+		}
+		
+		function save_photo() {
+			// actually snap photo (from preview freeze) and display it
+			Webcam.snap( function(data_uri) {
+				// display results in page
+				document.getElementById('results').innerHTML = 
+					'<h2>Your Previous Photo:</h2>' + 
+					'<img src="'+data_uri+'"/>';
+				// store the data uri of the photo to our hidden field
+				document.getElementById("imgData").value = data_uri;
+				//console.log(data_uri);
+
+				// swap buttons back
+				document.getElementById('pre_take_buttons').style.display = '';
+				document.getElementById('post_take_buttons').style.display = 'none';
+			} );
+		}
+
 	</script>
 	
 	
