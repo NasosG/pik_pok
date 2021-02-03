09704638c3f7843f3dc0bffeed562d6086ad00bb
Thanasis Galatis <gnasos219@gmail.com>
2021-02-04 00:35:29 +0200

    Update IPMAC_addresses.php

--- db/IPMAC_addresses.php
+++ db/IPMAC_addresses.php
@@ -28,53 +28,47 @@ function getBrowser()
     $version = "";
     $is_mobile = false;
 
-    //First get the platform?
-	if (preg_match('/android/i', $u_agent)) {
+    //First get the platform
+    if (preg_match('/android/i', $u_agent)) {
         $platform = 'android';
         $is_mobile = true;
     }
-	else if (preg_match("/(avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $u_agent)) {
+    else if (preg_match("/(avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $u_agent)) {
         $platform = 'non android mobile';
         $is_mobile = true;
     }
-	else if (preg_match('/windows|win32/i', $u_agent)) {
+    else if (preg_match('/windows|win32/i', $u_agent)) {
         $platform = 'windows';
     }
-	else if (preg_match('/macintosh|mac os x/i', $u_agent)) {
+    else if (preg_match('/macintosh|mac os x/i', $u_agent)) {
         $platform = 'mac';
     }
     else if (preg_match('/linux/i', $u_agent)) {
         $platform = 'linux';
     }
     
-    // Next get the name of the useragent yes seperately and for good reason
-    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-    {
+    // Next get the name of the user agent separately
+    if (preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i', $u_agent)) {
         $bname = 'Internet Explorer';
         $ub = "MSIE";
     }
-    else if(preg_match('/Firefox/i',$u_agent))
-    {
+    else if (preg_match('/Firefox/i', $u_agent)) {
         $bname = 'Mozilla Firefox';
         $ub = "Firefox";
     }
-    else if(preg_match('/Chrome/i',$u_agent))
-    {
+    else if (preg_match('/Chrome/i', $u_agent)) {
         $bname = 'Google Chrome';
         $ub = "Chrome";
     }
-    else if(preg_match('/Safari/i',$u_agent))
-    {
+    else if (preg_match('/Safari/i', $u_agent)) {
         $bname = 'Apple Safari';
         $ub = "Safari";
     }
-    else if(preg_match('/Opera/i',$u_agent))
-    {
+    else if (preg_match('/Opera/i', $u_agent)) {
         $bname = 'Opera';
         $ub = "Opera";
     }
-    else if(preg_match('/Netscape/i',$u_agent))
-    {
+    else if (preg_match('/Netscape/i', $u_agent)) {
         $bname = 'Netscape';
         $ub = "Netscape";
     }
@@ -92,7 +86,7 @@ function getBrowser()
     if ($i != 1) {
         //we will have two since we are not using 'other' argument yet
         //see if version is before or after the name
-        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)) {
             $version = $matches['version'][0];
         }
         else {
@@ -111,7 +105,7 @@ function getBrowser()
         'name'      => $bname,
         'version'   => $version,
         'platform'  => $platform,
-        'pattern'    => $pattern
+        'pattern'   => $pattern
     );
 }
 
@@ -121,7 +115,5 @@ function getBrowser()
 // lines below for debugging purposes
 // $yourbrowser= "Your browser: " . $ua['name'] . " " . $ua['version'] . " on " .$ua['platform'] ;
 // print_r($yourbrowser);
-
-
 
 ?>
