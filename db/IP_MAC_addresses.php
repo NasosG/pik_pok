3027fcd4644d7930e0ef6e5a0e5678ba790a8852
Thanasis Galatis <gnasos219@gmail.com>
2021-02-12 01:02:32 +0200

    code refactoring

--- db/IP_MAC_addresses.php
+++ db/IP_MAC_addresses.php
@@ -1,98 +1,114 @@
 <?php
-// PHP program to get IP address of client 
+// $IP stores the ip address of client
 $IP = $_SERVER['REMOTE_ADDR'];
 // $mac = shell_exec("arp -a ".escapeshellarg($_SERVER['REMOTE_ADDR'])." | grep -o -E '(:xdigit:{1,2}:){5}:xdigit:{1,2}'");
-// $IP stores the ip address of client 
-// echo "Client's IP address is: $IP"; 
 
-// Print the ip address of client 
-
-// PHP code to get the MAC address of Client 
+// get the MAC address of Client by storing 'getmac' value in $MAC -> not the best method
 $MAC = exec('getmac');
 
-// Storing 'getmac' value in $MAC 
+/*
+ * Updating $MAC value using strtok function,
+ * strtok is used to split the string into tokens, split character of strtok is defined as a space
+ * because getmac returns transport name after MAC address
+ * */
 $MAC = strtok($MAC, ' ');
 
-// Updating $MAC value using strtok function,  
-// strtok is used to split the string into tokens 
-// split character of strtok is defined as a space 
-// because getmac returns transport name after MAC address
-// echo "MAC address of client is: $MAC"; 
-
-function getBrowser()
+function getBrowser(): array
 {
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bname = 'Unknown';
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $browser_name = 'Unknown';
+    $user_browser = 'Unknown';
     $platform = 'Unknown';
-    $version = "";
     $is_mobile = false;
 
     //First get the platform
-    if (preg_match('/android/i', $u_agent)) {
+    $platform_details = getPlatform($platform, $is_mobile, $user_agent);
+    $platform = $platform_details[0];
+    $is_mobile = $platform_details[1];
+
+    // Next get the name of the user agent separately
+    $user_agent_name = getUserAgentName($browser_name, $user_browser, $user_agent);
+    $browser_name = $user_agent_name[0];
+    $user_browser = $user_agent_name[1];
+
+    // finally get the correct version number
+    $version_pattern = getVersion($user_browser, $user_agent);
+    $version = $version_pattern[0];
+    $pattern = $version_pattern[1];
+
+    return array(
+        'is_mobile' => $is_mobile,
+        'name' => $browser_name,
+        'version' => $version,
+        'platform' => $platform,
+        'pattern' => $pattern
+    );
+}
+
+function getPlatform($platform, $is_mobile, $user_agent): array
+{
+    if (preg_match('/android/i', $user_agent)) {
         $platform = 'android';
         $is_mobile = true;
-    }
-    else if (preg_match("/(avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $u_agent)) {
+    } else if (preg_match("/(avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $user_agent)) {
         $platform = 'non android mobile';
         $is_mobile = true;
-    }
-    else if (preg_match('/windows|win32/i', $u_agent)) {
+    } else if (preg_match('/windows|win32/i', $user_agent)) {
         $platform = 'windows';
-    }
-    else if (preg_match('/macintosh|mac os x/i', $u_agent)) {
+    } else if (preg_match('/macintosh|mac os x/i', $user_agent)) {
         $platform = 'mac';
-    }
-    else if (preg_match('/linux/i', $u_agent)) {
+    } else if (preg_match('/linux/i', $user_agent)) {
         $platform = 'linux';
     }
 
-    // Next get the name of the user agent separately
-    if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
-        $bname = 'Internet Explorer';
-        $ub = "MSIE";
-    }
-    else if (preg_match('/Firefox/i', $u_agent)) {
-        $bname = 'Mozilla Firefox';
-        $ub = "Firefox";
-    }
-    else if (preg_match('/Chrome/i', $u_agent)) {
-        $bname = 'Google Chrome';
-        $ub = "Chrome";
-    }
-    else if (preg_match('/Safari/i', $u_agent)) {
-        $bname = 'Apple Safari';
-        $ub = "Safari";
-    }
-    else if (preg_match('/Opera/i', $u_agent)) {
-        $bname = 'Opera';
-        $ub = "Opera";
-    }
-    else if (preg_match('/Netscape/i', $u_agent)) {
-        $bname = 'Netscape';
-        $ub = "Netscape";
+    return array($platform, $is_mobile);
+}
+
+function getUserAgentName($browser_name, $user_browser, $user_agent): array
+{
+    if (preg_match('/MSIE/i', $user_agent) && !preg_match('/Opera/i', $user_agent)) {
+        $browser_name = 'Internet Explorer';
+        $user_browser = "MSIE";
+    } else if (preg_match('/Firefox/i', $user_agent)) {
+        $browser_name = 'Mozilla Firefox';
+        $user_browser = "Firefox";
+    } else if (preg_match('/Chrome/i', $user_agent)) {
+        $browser_name = 'Google Chrome';
+        $user_browser = "Chrome";
+    } else if (preg_match('/Safari/i', $user_agent)) {
+        $browser_name = 'Apple Safari';
+        $user_browser = "Safari";
+    } else if (preg_match('/Opera/i', $user_agent)) {
+        $browser_name = 'Opera';
+        $user_browser = "Opera";
+    } else if (preg_match('/Netscape/i', $user_agent)) {
+        $browser_name = 'Netscape';
+        $user_browser = "Netscape";
     }
 
-    // finally get the correct version number
-    $known = array('Version', $ub, 'other');
+    return array($browser_name, $user_browser);
+}
+
+function getVersion($user_browser, $user_agent): array
+{
+    $known = array('Version', $user_browser, 'other');
     $pattern = '#(?<browser>' . join('|', $known) .
         ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $u_agent, $matches)) {
+    if (!preg_match_all($pattern, $user_agent, $matches)) {
         // we have no matching number just continue
     }
 
-    // see how many we have
+    // see how many do we have
     $i = count($matches['browser']);
     if ($i != 1) {
         //we will have two since we are not using 'other' argument yet
         //see if version is before or after the name
-        if (strripos($u_agent, "Version") < strripos($u_agent, $ub)) {
+        if (strripos($user_agent, "Version") < strripos($user_agent, $user_browser)) {
             $version = $matches['version'][0];
-        }
-        else {
+        } else {
             $version = $matches['version'][1];
         }
-    }
-    else {
+    } else {
         $version = $matches['version'][0];
     }
 
@@ -101,13 +117,7 @@ function getBrowser()
         $version = "?";
     }
 
-    return array(
-        'is_mobile' => $is_mobile,
-        'name' => $bname,
-        'version' => $version,
-        'platform' => $platform,
-        'pattern' => $pattern
-    );
+    return array($version, $pattern);
 }
 
 ?>
