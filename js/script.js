8ce7372c00f929a0e78172fe6972bb5b5706e927
Thanasis Galatis <gnasos219@gmail.com>
2021-03-21 05:22:33 +0200

    code beautification

--- js/script.js
+++ js/script.js
@@ -1,34 +1,36 @@
-$(window).on("load", function() {
+$(window).on("load", function () {
     "use strict";
 
     //  == CHANGE STATUS(ONLINE/OFFLINE) FUNCTIONALITY ==
 
-    $("#c5").on("click", function() {
+    $("#c5").on("click", function () {
         $.ajax({
             type: "POST",
             url: './db/handle_status.php',
-            data:{action:'set_status', text:'online'},
-            success:function() {}
+            data: {action: 'set_status', text: 'online'},
+            success: function () {
+            }
         });
     });
 
-    $("#c6").on("click", function() {
+    $("#c6").on("click", function () {
         $.ajax({
             type: "POST",
             url: './db/handle_status.php',
-            data:{action:'set_status', text:'offline'},
-            success:function() {}
+            data: {action: 'set_status', text: 'offline'},
+            success: function () {
+            }
         });
     });
 
     //  ============= POST PROJECT POPUP FUNCTION =========
 
-    $(".post_project").on("click", function(){
+    $(".post_project").on("click", function () {
         $(".post-popup.pst-pj").addClass("active");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".post-project > a").on("click", function(){
+    $(".post-project > a").on("click", function () {
         $(".post-popup.pst-pj").removeClass("active");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -36,12 +38,12 @@ $(window).on("load", function() {
 
     //  ============= POST JOB POPUP FUNCTION =========
 
-    $(".post-jb").on("click", function(){
+    $(".post-jb").on("click", function () {
         $(".post-popup.job_post").addClass("active");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".post-project > a").on("click", function(){
+    $(".post-project > a").on("click", function () {
         $(".post-popup.job_post").removeClass("active");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -49,34 +51,34 @@ $(window).on("load", function() {
 
     //  ============= SIGNIN CONTROL FUNCTION =========
 
-    $('.sign-control li').on("click", function(){
+    $('.sign-control li').on("click", function () {
         var tab_id = $(this).attr('data-tab');
         $('.sign-control li').removeClass('current');
         $('.sign_in_sec').removeClass('current');
         $(this).addClass('current animated fadeIn');
-        $("#"+tab_id).addClass('current animated fadeIn');
+        $("#" + tab_id).addClass('current animated fadeIn');
         return false;
     });
 
     //  ============= SIGNIN TAB FUNCTIONALITY =========
 
-    $('.signup-tab ul li').on("click", function(){
+    $('.signup-tab ul li').on("click", function () {
         var tab_id = $(this).attr('data-tab');
         $('.signup-tab ul li').removeClass('current');
         $('.dff-tab').removeClass('current');
         $(this).addClass('current animated fadeIn');
-        $("#"+tab_id).addClass('current animated fadeIn');
+        $("#" + tab_id).addClass('current animated fadeIn');
         return false;
     });
 
     //  ============= SIGNIN SWITCH TAB FUNCTIONALITY =========
 
-    $('.tab-feed ul li').on("click", function(){
+    $('.tab-feed ul li').on("click", function () {
         var tab_id = $(this).attr('data-tab');
         $('.tab-feed ul li').removeClass('active');
         $('.product-feed-tab').removeClass('current');
         $(this).addClass('active animated fadeIn');
-        $("#"+tab_id).addClass('current animated fadeIn');
+        $("#" + tab_id).addClass('current animated fadeIn');
         return false;
     });
 
@@ -89,12 +91,12 @@ $(window).on("load", function() {
 
     //  ============= OVERVIEW EDIT FUNCTION =========
 
-    $(".overview-open").on("click", function(){
+    $(".overview-open").on("click", function () {
         $("#overview-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#overview-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -102,12 +104,12 @@ $(window).on("load", function() {
 
     //  ============= EXPERIENCE EDIT FUNCTION =========
 
-    $(".exp-bx-open").on("click", function(){
+    $(".exp-bx-open").on("click", function () {
         $("#experience-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#experience-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -115,12 +117,12 @@ $(window).on("load", function() {
 
     //  ============= EDUCATION EDIT FUNCTION =========
 
-    $(".ed-box-open").on("click", function(){
+    $(".ed-box-open").on("click", function () {
         $("#education-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#education-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -128,12 +130,12 @@ $(window).on("load", function() {
 
     //  ============= LOCATION EDIT FUNCTION =========
 
-    $(".lct-box-open").on("click", function(){
+    $(".lct-box-open").on("click", function () {
         $("#location-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#location-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -141,12 +143,12 @@ $(window).on("load", function() {
 
     //  ============= SKILLS EDIT FUNCTION =========
 
-    $(".skills-open").on("click", function(){
+    $(".skills-open").on("click", function () {
         $("#skills-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#skills-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -154,12 +156,12 @@ $(window).on("load", function() {
 
     //  ============= ESTABLISH EDIT FUNCTION =========
 
-    $(".esp-bx-open").on("click", function(){
+    $(".esp-bx-open").on("click", function () {
         $("#establish-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#establish-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -167,12 +169,12 @@ $(window).on("load", function() {
 
     //  ============= CREATE PORTFOLIO FUNCTION =========
 
-    $(".portfolio-btn > a").on("click", function(){
+    $(".portfolio-btn > a").on("click", function () {
         $("#create-portfolio").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#create-portfolio").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -180,12 +182,12 @@ $(window).on("load", function() {
 
     //  ============= EMPLOYEE EDIT FUNCTION =========
 
-    $(".emp-open").on("click", function(){
+    $(".emp-open").on("click", function () {
         $("#total-employes").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#total-employes").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
@@ -193,100 +195,95 @@ $(window).on("load", function() {
 
     //  =============== Ask a Question Popup ============
 
-    $(".ask-question").on("click", function(){
+    $(".ask-question").on("click", function () {
         $("#question-box").addClass("open");
         $(".wrapper").addClass("overlay");
         return false;
     });
-    $(".close-box").on("click", function(){
+    $(".close-box").on("click", function () {
         $("#question-box").removeClass("open");
         $(".wrapper").removeClass("overlay");
         return false;
     });
 
+    //  ============== ChatBox ==============
 
-    //  ============== ChatBox ============== 
-
-
-    $(".chat-mg").on("click", function(){
+    $(".chat-mg").on("click", function () {
         $(this).next(".conversation-box").toggleClass("active");
         return false;
     });
-    $(".close-chat").on("click", function(){
+    $(".close-chat").on("click", function () {
         $(".conversation-box").removeClass("active");
         return false;
     });
 
     //  ================== Edit Options Function =================
 
-
-    $(".ed-opts-open").on("click", function(){
+    $(".ed-opts-open").on("click", function () {
         $(this).next(".ed-options").toggleClass("active");
         return false;
     });
 
-
     // needs some testing
-    $(".close-ed-opts").on("click", function(){
+    $(".close-ed-opts").on("click", function () {
         $(".ed-options").removeClass("active");
         return false;
     });
 
-
     // ============== Menu Script =============
 
-    $(".menu-btn > a").on("click", function(){
+    $(".menu-btn > a").on("click", function () {
         $("nav").toggleClass("active");
         return false;
     });
 
-
     //  ============ Notifications Open =============
 
-    $(".not-box-open").on("click", function(){$("#message").hide();
+    $(".not-box-open").on("click", function () {
+        $("#message").hide();
         $(".user-account-settingss").hide();
         $(this).next("#notification").toggle();
     });
 
-     //  ============ Messages Open =============
+    //  ============ Messages Open =============
 
-    $(".not-box-openm").on("click", function(){$("#notification").hide();
+    $(".not-box-openm").on("click", function () {
+        $("#notification").hide();
         $(".user-account-settingss").hide();
         $(this).next("#message").toggle();
     });
 
-
     // ============= User Account Setting Open ===========
-	/*
-$(".user-info").on("click", function(){$("#users").hide();
+
+    /*
+    $(".user-info").on("click", function(){$("#users").hide();
         $(".user-account-settingss").hide();
         $(this).next("#notification").toggle();
     });
-    
-	*/
-	$( ".user-info" ).click(function() {
-  $( ".user-account-settingss" ).slideToggle( "fast");
-	  $("#message").not($(this).next("#message")).slideUp();
-	  $("#notification").not($(this).next("#notification")).slideUp();
-    // Animation complete.
-  });
- 
+    */
+
+    $(".user-info").click(function () {
+        $(".user-account-settingss").slideToggle("fast");
+        $("#message").not($(this).next("#message")).slideUp();
+        $("#notification").not($(this).next("#notification")).slideUp();
+        // Animation complete.
+    });
 
     //  ============= FORUM LINKS MOBILE MENU FUNCTION =========
 
-    $(".forum-links-btn > a").on("click", function(){
+    $(".forum-links-btn > a").on("click", function () {
         $(".forum-links").toggleClass("active");
         return false;
     });
-    $("html").on("click", function(){
+    $("html").on("click", function () {
         $(".forum-links").removeClass("active");
     });
-    $(".forum-links-btn > a, .forum-links").on("click", function(){
+    $(".forum-links-btn > a, .forum-links").on("click", function () {
         e.stopPropagation();
     });
 
 
-	/*
+    /*
     //  ============= PORTFOLIO SLIDER FUNCTION =========
 
     $('.profiles-slider').slick({
@@ -329,11 +326,6 @@ $(".user-info").on("click", function(){$("#users").hide();
 
 
     });
-	*/
-
-
-
+    */
 
 });
-
-
